66b649250f98463dd41c8fcbf32fd489b50f99de
rebixpl <rebixcraft@gmail.com>
2021-09-19 19:26:15 +0200

    update #1

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -3,10 +3,39 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
+	PrintLine(TEXT("Welcome to Bull & Cows!"));
+	PrintLine(TEXT("Guess the 4 letter word!")); //TODO: Magic Number Remove!
+	PrintLine(TEXT("Press Enter to continue..."));
+
+	SetupGame();
+
+	// Prompt Player for Guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+	ClearScreen();
 
+	if (Input == HiddenWord) {
+		PrintLine(TEXT("You have Won!"));
+	}
+	else {
+		PrintLine(TEXT("You have Lost!"));
+		P_Lives--;
+	}
+
+	if (P_Lives > 0) {
+		// Ask for a guess
+	}
+	else {
+		PrintLine(TEXT("You have 0 Lives Left. YOU LOST."));
+		// Prompt to play again
+		// Show Hidden word
+	}
+}
+
+void UBullCowCartridge::SetupGame() {
+	HiddenWord = TEXT("cake");
+	P_Lives = P_LivesMax;
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,16 +6,19 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	void SetupGame();
 
 	// Your declarations go below!
-	private:
-	
+private:
+	FString HiddenWord;
+	const int32 P_LivesMax = 4;
+	int32 P_Lives;
 };
